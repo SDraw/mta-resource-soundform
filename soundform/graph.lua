f1edb6be9dd87fecafab37af341c18d50f38c0ac
SDraw <sdraw5130@gmail.com>
2015-06-05 05:28:18 +0300

    Fixes and new stuff
    Added FFT and wave representation

--- soundform/graph.lua
+++ soundform/graph.lua
@@ -31,16 +31,14 @@ function mplayer.playSoundFile(str1)
 end
 function mplayer.generateHeights()
     local left,right = getSoundLevelData(mplayer.element)
+    mplayer.heights[mplayer.cycle+1] = {}
     if(left ~= false) then
-        mplayer.heights[mplayer.cycle+1] = {}
         mplayer.heights[mplayer.cycle+1].left = math.floor(128*left/32768)
         mplayer.heights[mplayer.cycle+1].right = math.floor(64*right/32768)
     else
-        mplayer.heights[mplayer.cycle+1] = {}
         mplayer.heights[mplayer.cycle+1].left = 0
         mplayer.heights[mplayer.cycle+1].right = 0
     end
-    outputDebugString("Got for cycle "..(mplayer.cycle+1))
     mplayer.cycle = mplayer.cycle+1
     if(mplayer.cycle ~= 1280) then
         setSoundPosition(mplayer.element,mplayer.song.length/1280*(mplayer.cycle))
@@ -68,13 +66,22 @@ end
 function mplayer.onClientRender()
     dxDrawImage(0,0,mplayer.screen.x,mplayer.screen.y,"background.png")
     dxDrawRectangle(mplayer.screen.x/2-650,mplayer.screen.y/2-138,1300,212,tocolor(0,0,0,63))
+    dxDrawRectangle(mplayer.screen.x/2-650,mplayer.screen.y/2+81,276,116,tocolor(0,0,0,63))
+    dxDrawRectangle(mplayer.screen.x/2-368,mplayer.screen.y/2+81,276,116,tocolor(0,0,0,63))
     if(mplayer.generating == true) then
         for i=1,256 do
             local rs = getTickCount()/1000
             local ft = math.floor(math.abs(math.sin(rs+i*2))*128)
+            --Volume
             dxDrawRectangle(mplayer.screen.x/2-640+5*(i-1),mplayer.screen.y/2-ft,4,ft,tocolor(255,85,0))
-            ft = math.floor(math.abs(math.sin(rs+i*2))*64)
+            ft = math.floor(ft/2)
             dxDrawRectangle(mplayer.screen.x/2-640+5*(i-1),mplayer.screen.y/2,4,ft,tocolor(255,187,153))
+            --FFT
+            dxDrawRectangle(mplayer.screen.x/2-640+(i-1),mplayer.screen.y/2+155-ft,1,ft,tocolor(255,85,0))
+            ft = math.floor(ft/2)
+            dxDrawRectangle(mplayer.screen.x/2-640+(i-1),mplayer.screen.y/2+155,1,ft,tocolor(255,187,153))
+            --Wave
+            dxDrawRectangle(mplayer.screen.x/2-358+(i-1),mplayer.screen.y/2+139-math.floor(48*math.sin(rs+math.rad(i))),1,1,tocolor(255,187,153))
         end
         dxDrawRectangle(mplayer.screen.x/2-50,mplayer.screen.y/2-20,100,40,tocolor(0,0,0,127))
         dxDrawText("GENERATING\n"..math.floor(mplayer.cycle/1280*100).."%",mplayer.screen.x/2-44,mplayer.screen.y/2-19,mplayer.screen.x/2+46,mplayer.screen.y/2+21,tocolor(0,0,0,127),1,mplayer.fonts[1],"center","center")
@@ -82,11 +89,14 @@ function mplayer.onClientRender()
         
     else
         if(mplayer.playing == true) then
+            --Title
             dxDrawText(mplayer.song.title.."\n"..mplayer.song.artist.."\n"..mplayer.song.album,mplayer.screen.x/2+78,mplayer.screen.y/2-277,mplayer.screen.x/2+641,mplayer.screen.y/2-141,tocolor(0,0,0,127),1,mplayer.fonts[2],"right","top",false,false,false,false,false)
             dxDrawText(mplayer.song.title.."\n"..mplayer.song.artist.."\n"..mplayer.song.album,mplayer.screen.x/2+77,mplayer.screen.y/2-278,mplayer.screen.x/2+640,mplayer.screen.y/2-142,tocolor(255,255,255),1,mplayer.fonts[2],"right","top",false,false,false,false,false)
+            --Calculation vars
             local pos = getSoundPosition(mplayer.element)
             local size = math.floor(1280*pos/mplayer.song.length)
             local _,lp = math.modf(1280*pos/mplayer.song.length)
+            --Volume
             for i=0,size-1 do
                 dxDrawRectangle(mplayer.screen.x/2-640+i,mplayer.screen.y/2-mplayer.heights[i+1].left,1,mplayer.heights[i+1].left,tocolor(255,85,0))
                 dxDrawRectangle(mplayer.screen.x/2-640+i,mplayer.screen.y/2,1,mplayer.heights[i+1].right,tocolor(255,187,153))
@@ -100,13 +110,35 @@ function mplayer.onClientRender()
                 dxDrawRectangle(mplayer.screen.x/2-640+size,mplayer.screen.y/2-mplayer.heights[size+1].left,1,mplayer.heights[size+1].left,tocolor(255,85,0,math.floor(255*lp)))
                 dxDrawRectangle(mplayer.screen.x/2-640+size,mplayer.screen.y/2,1,mplayer.heights[size+1].right,tocolor(255,187,153,math.floor(255*lp)))
             end
-            dxDrawRectangle(mplayer.screen.x/2-640+size-49,mplayer.screen.y/2-128+98,98,30,tocolor(0,0,0,63))
-            dxDrawRectangle(mplayer.screen.x/2-640+size-50,mplayer.screen.y/2-128+98,1,30,tocolor(0,0,0,math.floor(63*(1.0-lp))))
-            dxDrawRectangle(mplayer.screen.x/2-640+size+49,mplayer.screen.y/2-128+98,1,30,tocolor(0,0,0,math.floor(63*lp)))
+            dxDrawRectangle(mplayer.screen.x/2-640+size-49,mplayer.screen.y/2-128+98,98,30,tocolor(0,0,0,95))
+            dxDrawRectangle(mplayer.screen.x/2-640+size-50,mplayer.screen.y/2-128+98,1,30,tocolor(0,0,0,math.floor(95*(1.0-lp))))
+            dxDrawRectangle(mplayer.screen.x/2-640+size+49,mplayer.screen.y/2-128+98,1,30,tocolor(0,0,0,math.floor(95*lp)))
             dxDrawText(tostring(math.floor(pos/60))..":"..string.format("%02d",(math.floor(pos)%60)).." | "..math.floor(mplayer.song.length/60)..":"..string.format("%02d",(math.floor(mplayer.song.length)%60)),mplayer.screen.x/2-640+size-50-(1.0-lp),mplayer.screen.y/2-128+98,mplayer.screen.x/2-640+size+50+lp,mplayer.screen.y/2-128+128,tocolor(255,255,255),1,"default","center","center",false,false,false,false,true)
+            --FFT
+            local fft = getSoundFFTData(mplayer.element,2048,257)
+            if(fft ~= false) then
+                for i=1,#fft do
+                    local ol = math.floor(math.sqrt(math.sqrt(fft[i]))*64)
+                    dxDrawRectangle(mplayer.screen.x/2-640+(i-1),mplayer.screen.y/2+155-ol,1,ol,tocolor(255,85,0))
+                    ol = math.floor(ol/2)
+                    dxDrawRectangle(mplayer.screen.x/2-640+(i-1),mplayer.screen.y/2+155,1,ol,tocolor(255,187,153))
+                end
+            end
+            --Wave
+            local wave = getSoundWaveData(mplayer.element,512)
+            if(wave) then
+                for i=1,127 do
+                    local ol = math.floor(48*wave[(i-1)*2+1])
+                    local el = math.floor(48*wave[(i-1)*2+2])
+                    dxDrawLine(mplayer.screen.x/2-358+(i-1)*2,mplayer.screen.y/2+139+ol,mplayer.screen.x/2-358+(i-1)*2+1,mplayer.screen.y/2+139+el,tocolor(255,187,153))
+                end
+            end
         else
             dxDrawRectangle(mplayer.screen.x/2-640,mplayer.screen.y/2-1,1280,1,tocolor(255,85,0))
             dxDrawRectangle(mplayer.screen.x/2-640,mplayer.screen.y/2,1280,1,tocolor(255,187,153))
+            dxDrawRectangle(mplayer.screen.x/2-640,mplayer.screen.y/2+154,256,1,tocolor(255,85,0))
+            dxDrawRectangle(mplayer.screen.x/2-640,mplayer.screen.y/2+155,256,1,tocolor(255,187,153))
+            dxDrawRectangle(mplayer.screen.x/2-358,mplayer.screen.y/2+129,256,1,tocolor(255,187,153))
         end
     end
 end
